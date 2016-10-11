e35eaaff202c51919e52618eba9675c90f8fa2c5
xvw <xavier.vdw@gmail.com>
2016-10-11 11:13:01 +0200

    progress on standard template

--- slider_src/slideshow.ml
+++ slider_src/slideshow.ml
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
-*)
+*) 
 
 open Quasar
 
@@ -43,8 +43,42 @@ struct
       | None -> ignore (set_label slide)
       | Some x -> label := Some x
 
+  let perform_label elt slides cursor =
+    let slide = List.nth slides !cursor in
+    match Element.get_data "label" slide with
+    | None -> ()
+    | Some value ->
+      Dom.appendChild
+        (Element.clean elt)
+        (Element.text value)
 
-  let update = QuaSlideshow.default_update
+  let hide_if_frontpage cursor =
+    let slides =
+      document##querySelectorAll (String.js ".quasar-removable")
+      |> Dom.list_of_nodeList
+    in 
+    if cursor = 0 then
+      List.iter (fun s -> ignore(Element.add_class "removable" s)) slides
+    else List.iter (fun s -> ignore (Element.remove_class "removable" s)) slides
+      
+
+  let update ~length ~slides cursor =
+    let open Element in
+    let _  = QuaSlideshow.default_update ~length ~slides cursor in
+    let () = match getById_opt "pagination" with
+      | None -> ()
+      | Some elt ->
+        Dom.appendChild
+          (clean elt)
+          (text $ Printf.sprintf "%d/%d" (!cursor + 1) length)
+    in
+    let () =
+      match getById_opt "label" with
+      | None -> ()
+      | Some elt -> perform_label elt slides cursor
+    in hide_if_frontpage !cursor
+
+  
                  
   let before  ~length ~slides cursor =
     QuaSlideshow.default_before
